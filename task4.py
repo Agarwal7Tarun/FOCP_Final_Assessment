c1a5ffa92efb9ed0042bc874c21bac0c91b34c12
Tarun <atarun21@tbc.edu.np>
2023-01-12 18:19:48 +0545

    first commit

---
+++ task4.py
@@ -0,0 +1,29 @@
+# FOCP FINAL TASK 4
+import sys
+if len(sys.argv) < 2:
+    print("Usage: caesar_cracker.py <encrypted_message_file>")
+    sys.exit(1)
+filename = sys.argv[1]
+validate= "the"
+try:
+    with open(filename, "r") as f:
+        encrypted_message = f.read()
+        possibility=1
+except IOError:
+    print("Cannot open '{}'. Sorry about that.".format(filename))
+    sys.exit(1)
+for shift in range(26):
+    decrypted_message = ""
+    for ch in encrypted_message:
+        if ch.isalpha():
+            if ch.isupper():
+                decrypted_message += chr((ord(ch) - ord("A") + shift) % 26 + ord("A"))
+            else:
+                decrypted_message += chr((ord(ch) - ord("a") + shift) % 26 + ord("a"))
+        else:
+            decrypted_message += ch
+    if validate in decrypted_message:
+        print("\nPossibility {} Shift {}: {}".format(possibility, shift, decrypted_message))
+        possibility+=1
+    else:
+        pass
